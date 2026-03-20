e1be0ebda20f617cd235eefd78bf1cfa4475a982
eliass <77859813+prodige93@users.noreply.github.com>
2026-03-20 15:56:01 +0100

    feat: setup express backend port 3300

---
+++ backend/server.ts
@@ -0,0 +1,19 @@
+import express = require("express");
+import cors = require("cors")
+
+//Configuration / initialisation du server Express
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+//Initialisation au lancement du serveur 
+app.get("/api", (_req, res) => {
+    console.log("Requête reçue sur /api")
+    res.send("API running 🚀")
+});
+
+//Écoute du server sur le port 3300
+app.listen(3300, () => {
+    console.log("Server running on port 3300 🚀");
+})
